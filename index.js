04654d5b83c307311acbc81a0e20c7a2f6e1c301
osxgirl <osxgirl@mac.com>
2021-03-04 18:44:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(array, fn) {
+    let newArray = [];
+    for (const element of array) {
+      newArray.push(fn(element));
+    }
+    return newArray;
+  }
+  
+  
+  function reduce(array, fn, startingPoint = 0) {
+    let total;
+    let counter;
+  
+    if (!!startingPoint) {
+      total = startingPoint;
+      counter = 0;
+    } else {
+      total = array[0];
+      counter = 1;
+    }
+  
+     for (let i = counter; i < array.length; i++) {
+       total = fn(total, array[i])
+     }
+    return total
+  }
